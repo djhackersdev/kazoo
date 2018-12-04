5e7863b9dadf593733c77980d55b3a4858b6de09
Tau <tau@nowhere>
2018-12-04 16:45:20 -0500

    Add stub matching service to accept HELLO

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const fs = require('fs')
 const http = require('http')
+const tls = require('tls')
 
 const allnet = require('./allnet')
+const matching = require('./matching')
 
 http.createServer(allnet).listen(80)
+
+tls.createServer(
+  {
+    key: fs.readFileSync('pki/matching.key'),
+    cert: fs.readFileSync('pki/matching.pem'),
+  },
+  matching
+).listen(32639)
