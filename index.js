08e8e9273a83adcbe50e8fca25be45f807c9c18b
Tau <tau@nowhere>
2018-12-28 13:35:17 -0500

    Add initial Protobuf-over-HTTPS services

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const fs = require('fs')
 const http = require('http')
+const https = require('https')
 const tls = require('tls')
 
 const allnet = require('./allnet')
 const matching = require('./matching')
+const svc = require('./svc')
 
 http.createServer(allnet).listen(80)
+
+https.createServer(
+  {
+    key: fs.readFileSync('pki/accepter.key'),
+    cert: fs.readFileSync('pki/accepter.pem'),
+  },
+  svc
+).listen(32635)
 
 tls.createServer(
   {
