531c591d1097f3a8969808d969e1254a7a776f46
Tau <tau@nowhere>
2019-02-07 15:46:19 -0500

    index: Re-enable dbaccess service
    Still poisons the game, need to figure out why.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { readFileSync } from "fs";
 import http = require("http");
+import https = require("https");
 import tls = require("tls");
 
 import allnet from "./allnet";
 import matching from "./matching";
+import svc from "./svc";
 
 http.createServer(allnet).listen(80);
 
@@ -13,8 +15,18 @@ tls
       key: readFileSync("pki/matching.key"),
       cert: readFileSync("pki/matching.pem"),
     },
-    matching,
+    matching
   )
   .listen(32639);
+
+https
+  .createServer(
+    {
+      key: readFileSync("pki/accepter.key"),
+      cert: readFileSync("pki/accepter.pem"),
+    },
+    svc
+  )
+  .listen(32635);
 
 console.log("Startup OK");
