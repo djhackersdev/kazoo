63a5476d7391b5fc782846381a45aa12efff5ce5
Tau <tau@nowhere>
2019-02-12 16:32:00 -0500

    svc: Final experiments for v3

--- src/svc.ts
+++ src/svc.ts
@@ -35,35 +35,37 @@ app.use(async function readStep(req, res, next) {
 });
 
 //
-// Generic success header
-//
-
-const header: v403db.IResponseHeader = {
-  success: true,
-  errcode: 0,
-  protover: 20110702,
-};
-
-//
 // Handlers
 //
 
 app.post("/v403/client/update", function(req, res) {
-  v403db.V403REQ_ClientUpdate.decode(req.body);
+  const reqp = v403db.V403REQ_ClientUpdate.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_ClientUpdate.encode({
-      header,
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
     }).finish()
   );
 });
 
 app.post("/v403/client/update-errlog", function(req, res) {
-  v403db.V403REQ_ClientUpdateErrlog.decode(req.body);
+  const reqp = v403db.V403REQ_ClientUpdateErrlog.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_ClientUpdateErrlog.encode({
-      header,
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
     }).finish()
   );
 });
@@ -72,19 +74,25 @@ app.post("/v403/shop/info", function(req, res) {
   // If this does not match the data returned by AllNET then we will receive
   // a ShopUpdate call to make it match.
 
-  v403db.V403REQ_ShopInfo.decode(req.body);
+  const reqp = v403db.V403REQ_ShopInfo.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_ShopInfo.encode({
-      header,
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
       battalionName: "batname",
       countryCode: "JPN",
-      areaId: 0,
-      regionId1: 0,
-      regionId2: 0,
+      areaId: 1,
+      regionId1: 7,
+      regionId2: 1,
       shopName: "asdf",
-      shopAddress: "",
-      score: 4,
+      shopAddress: "WXYZ",
+      /*score: 4,
       countryRank: 5,
       regionRank: 6,
       areaRank: 7,
@@ -92,65 +100,101 @@ app.post("/v403/shop/info", function(req, res) {
       controlFlag: 0,
       availableBanacoin: false,
       semLastUseDay: 9,
-      semUseCount: 10,
+      semUseCount: 10,*/
     }).finish()
   );
 });
 
 // Sent if we mess up the above
 app.post("/v403/shop/update", function(req, res) {
-  v403db.V403REQ_ShopUpdate.decode(req.body);
+  const reqp = v403db.V403REQ_ShopUpdate.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_ShopUpdate.encode({
-      header,
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
     }).finish()
   );
 });
 
 app.post("/v403/game/config", function(req, res) {
-  v403db.V403REQ_GameConfig.decode(req.body);
+  const reqp = v403db.V403REQ_GameConfig.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_GameConfig.encode({
-      header,
-      normalMission1: 0,
-      normalMission2: 0,
-      eventMission1: 0,
-      eventMission2: 0,
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
+      normalMission1: 1,
+      normalMission2: 2,
+      eventMission1: 1,
+      eventMission2: 2,
       eventOption: 0,
-      optionInfo1: 0,
-      optionInfo2: 0,
+      optionInfo1: 1,
+      optionInfo2: 2,
       eventText: "evtext",
       telop: "telop",
-      serverFlag1: 0,
-      serverFlag2: 0,
-      eventRegulation1: 0,
-      eventRegulation2: 0,
+      serverFlag1: 1,
+      serverFlag2: 1,
+      /*eventRegulation1: 10,
+      eventRegulation2: 11,
+      titleCampaignInfo: {
+        eventId: 1,
+        eventStampMax: 2,
+        eventTitleId: 3,
+      },
+      exmsCampaignInfo: {
+        campaignId: 4,
+        period: 5,
+        startDate: "20100101",
+      },
+      rentalmsCampaignInfo: [],
+      isBfday: false,
+      requiredClientVersion: 35707,*/
     }).finish()
   );
 });
 
 app.post("/v403/warevent/status", function(req, res) {
-  v403db.V403REQ_WarEventStatus.decode(req.body);
+  const reqp = v403db.V403REQ_WarEventStatus.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_WarEventStatus.encode({
-      header,
+      header: {
+        success: true,
+        errcode: 0,
+        // Note! Differs from others!
+        protover: v403db.ProtocolVersions_rev306.PROTOCOL_VERSION_REV306,
+      },
       eventId: 0,
-      // A whole shitload of optional fields. Let's try to disable this
-      // "war event".
     }).finish()
   );
 });
 
 app.post("/v403/notice/config", function(req, res) {
-  v403db.V403REQ_NoticeConfig.decode(req.body);
+  const reqp = v403db.V403REQ_NoticeConfig.decode(req.body);
+
+  console.log(JSON.stringify(reqp));
 
   res.send(
     v403db.V403RES_NoticeConfig.encode({
-      header,
-      // A repeated field listing update packages? follows
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
+      notice: [],
     }).finish()
   );
 });
@@ -159,17 +203,25 @@ app.post("/v403/clientlog/update", function(req, res) {
   const reqp = v403db.V403REQ_ClientlogUpdate.decode(req.body);
 
   for (const record of reqp.records) {
-    const chunks = record.logData.split(",");
-    const zbytes = Buffer.from(chunks[3], "base64");
-    const bytes = inflateSync(zbytes);
-    const ascii = bytes.toString("ascii");
+    try {
+      const chunks = record.logData.split(",");
+      const zbytes = Buffer.from(chunks[3], "base64");
+      const bytes = inflateSync(zbytes);
+      const ascii = bytes.toString("ascii");
 
-    console.log(ascii);
+      console.log(ascii);
+    } catch (e) {
+      console.log("bad record");
+    }
   }
 
   res.send(
     v403db.V403RES_ClientlogUpdate.encode({
-      header,
+      header: {
+        success: true,
+        errcode: 0,
+        protover: v403db.ProtocolVersions.PROTOCOL_VERSION,
+      },
     }).finish()
   );
 });
