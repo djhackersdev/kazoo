6f7105de15691cb0fef61d2ab3aff196f747fb6d
Tau <tau@nowhere>
2019-07-22 22:11:27 -0400

    Delete "captures"
    They live on in Git history if we really need them.

--- live/naominet.jp/unz.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const fs = require("fs")
-const zlib = require("zlib")
-
-const zbytes = fs.readFileSync(process.argv[2]);
-const bytes = zlib.inflateSync(zbytes);
-
-fs.writeFileSync(process.argv[3], bytes);
